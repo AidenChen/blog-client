941e7effca005c2a1b2e3b8a9c8ef620bfa69cd3
Aiden <chenscrocker@hotmail.com>
2017-12-18 20:19:56 +0800

    删除无用代码

--- src/api/tag.js
+++ src/api/tag.js
@@ -1,15 +1,6 @@
 import Axios from 'axios';
 
 export default {
-  create(name) {
-    return Axios.post('/api/tags', { name });
-  },
-  destroy(id) {
-    return Axios.delete(`/api/tags/${id}`);
-  },
-  update(id, tag) {
-    return Axios.put(`/api/tags/${id}`, tag);
-  },
   index() {
     return Axios.get('/api/tags');
   }
